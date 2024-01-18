5d9167c21b976f2b2d9907f52675aba4c5073385
rpalrod0912 <rafapr0001@gmail.com>
2024-01-18 17:01:11 +0100

    basic calculator angular ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
 
+const operations = ['+', '-', '*', '/'];
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  numbers: string = '';
+  result: number = 0;
 
   constructor() {}
 
+  addNumber(num: number | string): void {
+    console.log(this.numbers.slice(-1).toString());
+    if (!operations.includes(this.numbers.slice(-1).toString())) {
+      this.numbers = `${this.numbers}${num}`;
+    } else if (!operations.includes(num.toString())) {
+      this.numbers = `${this.numbers}${num}`;
+    }
+  }
+
+  calculate(): void {
+    if (operations.includes(this.numbers.slice(-1).toString())) {
+      this.numbers = this.numbers.slice(0, -1);
+    }
+    this.result = eval(this.numbers);
+  }
+
+  deleteDigit(): void {
+    this.numbers = this.numbers.slice(0, -1);
+  }
+
+  clear(): void {
+    this.numbers = '';
+    this.result = 0;
+  }
 }
